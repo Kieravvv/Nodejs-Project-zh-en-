0a7b3f34858acdb75845663cab31f23fb2fc924b
Kieravvv <93302642+Kieravvv@users.noreply.github.com>
2022-03-11 17:02:12 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require("http");
+const fs = require("fs"); 
+const { clear } = require("console");
+const port = 3000;
+const ip ="127.0.0.1";
+
+const sendResponse = (filename, statusCode,response) =>{
+    fs.readFile(`./${filename}`, (error,data)=>{
+          if(error){
+            response.statusCode = 500;
+            response.setHeader("Content-Type", "text/plain");
+            response.end("Sorry, internal error");
+          }else{
+            response.statusCode = statusCode;
+            response.setHeader("Content-Type", "text/html");
+            response.end(data);
+          }
+    });
+};
+
+const server = http.createServer((request,response)=>{
+    const method = request.method;
+    let url = request.url;
+
+    if(method === "GET"){
+        const requestUrl = new URL(url,`http://${ip}:${port}`);
+        url=requestUrl.pathname;
+        const lang = requestUrl.searchParams.get("lang");
+        let selector;
+
+        if(lang === null || lang === "en"){
+            selector = "";
+        }else if(lang === "zh"){
+            selector ="-zh";
+        } else{
+            selector = "";
+        }
+
+        if(url === "/"){
+            sendResponse(`index${selector}.html`, 200, response);
+        }else if(url === "/about.html"){
+            sendResponse(`about${selector}.html`, 200, response);
+        }else{
+            sendResponse(`404${selector}.html`, 404, response);
+        }
+    }else{
+
+    }
+    //response.end("Hello from NodeJS server2"); 
+});
+
+server.listen(port,ip,()=>{
+    console.log(`Server is running at http://${ip}:${port}`);
+});
